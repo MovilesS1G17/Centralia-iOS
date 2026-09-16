d17c84b2bce10b33a97522ad29d30e4756207f3f
Dav1d10 <d.carol@uniandes.edu.co>
2026-09-16 17:56:35 -0500

    Initial Commit

---
+++ Centralia/CentraliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentraliaApp.swift
+//  Centralia
+//
+//  Created by David Caro on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CentraliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
